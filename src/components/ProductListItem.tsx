7acc0b5c06452bbe1171238614f5abfa2e73eea7
yanirc1981 <cerzurc@hotmail.com>
2024-06-15 17:12:28 -0300

    Se inicia e-commers

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '@/constants/Colors';
+import { Product } from '@/types';
+
+export const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+    product: Product;
+};
+
+const ProductListItem = ({ product }:ProductListItemProps) =>{
+  return (
+    <View style={styles.container}>
+      <Image 
+      style={styles.image}
+      source={{uri: product.image || defaultPizzaImage}}
+      resizeMode='contain'
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  )
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+   backgroundColor:"white",
+   padding: 10,
+   borderRadius:20,
+   flex:1,
+   maxWidth:"50%"
+  },
+  image:{
+    width:'100%',
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical:10,
+  },
+  price:{
+    color:Colors.light.tint,
+    fontWeight:"bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
