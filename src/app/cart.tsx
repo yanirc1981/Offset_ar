486d9ba3412fb9b74df18b7fd1227886dafa1e4a
yanirc1981 <cerzurc@hotmail.com>
2024-06-18 16:34:08 -0300

    se termina items carrito

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,18 +1,21 @@
-import { View, Platform, FlatList} from 'react-native';
+import { View, Platform, FlatList, Text} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useCart } from '@/providers/CartProvider';
 import CartListItem from '@/components/CartListItem';
+import Button from '@/components/Button';
 
 
 const CartScreen = () => {
-    const {items} = useCart();
+    const {items, total} = useCart();
   return (
-    <View>
+    <View style={{padding:10}}>
         <FlatList
          data={items} 
          renderItem={({item})=><CartListItem cartItem={item}/>}
-         contentContainerStyle={{padding:10, gap:10}}
+         contentContainerStyle={{ gap:10}}
          /> 
+         <Text style={{fontSize: 20, marginTop:20, fontWeight:'500'}}>Total: ${total}</Text>
+         <Button text='Checkout'/>
       
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'}></StatusBar>
     </View>
