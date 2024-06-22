f66897f57711a4750cb7fd8ad332912de3f61ab8
yanirc1981 <cerzurc@hotmail.com>
2024-06-22 10:07:04 -0300

    se agrega sign in

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -17,6 +17,9 @@ const index = () =>{
             <Link href={'/(admin)'} asChild>
             <Button text="Admin"/>
             </Link>
+            <Link href={'/sign-in'} asChild>
+            <Button text="Sign in"/>
+            </Link>
         </View>
     )
 }
